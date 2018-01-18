3430a20b6f39649c96474547180904e74d60fc76
Joonas Aapro <joonaaa@ety-a2123b-12.edu.wks.metropolia.fi>
2018-01-18 15:52:03 +0200

    first commit

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,6 +9,10 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
   tulos  = 'Moro';
+  apitulos = 'Moro taas';
+  apiosoite = 'http://media.mw.metropolia.fi/wbma';
+  kuvaosoite = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  digitraffic = 'https://rata.digitraffic.fi/api/v1/trains';
   constructor(private http: HttpClient) { }
 
   getJson() {
@@ -20,8 +24,20 @@ export class HttpTestComponent implements OnInit {
       this.tulos = data.license;
     });
   }
+
+  getFromApi () {
+      //this.http.get(this.apiosoite + '/media').subscribe( data => {
+        //console.log(data[0].filename);
+        //this.apitulos = this.kuvaosoite + data[0].filename;
+          this.http.get(this.digitraffic).subscribe( data => {
+              console.log(data);
+              this.juna = data;
+      });
+  }
+
   ngOnInit() {
     this.getJson();
+    this.getFromApi();
   }
 
 }
